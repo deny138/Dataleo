ddcdda1e56e965c908acd1e26f88862d70630a9d
Deny <Deny@147.232.186.201>
2016-02-12 15:42:28 +0100

    stvrty commit - uz len jeden vypis pohladu ale divi v ukazkach

--- kontrolery/ZdrojeKontroler.php
+++ kontrolery/ZdrojeKontroler.php
@@ -9,6 +9,7 @@ class ZdrojeKontroler extends Kontroler {
         $this->hlavicka['titulok'] = 'Moje zdroje';
         //vytvorenie novej instancie ktora nam umozni vypis zdrojov
         $vypisZdrojov = new VypisZdrojov();
+        
         $zdroje = $vypisZdrojov->vratZdroje();
         $zdroje_ukazka = $vypisZdrojov->vratZdroj("4"); //TODO tak aby podla kliknuteho clanku v strednom okne bola zobrazena ukazka vpravo
         $this->data['zdroje'] = $zdroje; //aby sa dalo v pohlade pracovat s premennou zdroje
