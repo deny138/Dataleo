b462993df8995fb18253546a7f30de9fd4f8bec1
Deny <Deny@147.232.186.201>
2016-03-04 23:52:41 +0100

    siedmy commit- administracia hotova + grafika

--- kontrolery/ZdrojeKontroler.php
+++ kontrolery/ZdrojeKontroler.php
@@ -5,8 +5,8 @@ class ZdrojeKontroler extends Kontroler {
     public function spracuj($parametre) {
         //zabezpecenie aby v pohlade zdroje.phtml bol pristup k udajom o pouzivatelovi kvoli vypisu iba JEHO zdrojov
         $spravcaPouzivatelov = new SpravcaPouzivatelov();
-        $pouzivatel=$spravcaPouzivatelov->vratPouzivatela();
-       $pouzivatelovo_id=$pouzivatel['pouzivatel_id'];
+        $pouzivatel = $spravcaPouzivatelov->vratPouzivatela();
+        $pouzivatelovo_id = $pouzivatel['pouzivatel_id'];
 
         $id = 0;
         if (!empty($_GET['zdroj_id'])) {
@@ -18,14 +18,12 @@ class ZdrojeKontroler extends Kontroler {
         //metoda ktora vrati zdroje pre vypis  zoznamu vsetkych zdrojov pouzivatela, ktory je proihlaseny
         $zdroje = $vypisZdrojov->vratZdrojeSautorom($pouzivatelovo_id);
         //metoda ktora vrati zdroj podla zdroj_id a podla toho zobrazi ukazku
-        $zdroje_ukazka = $vypisZdrojov->vratZdrojPodlaZdrojId($id); 
+        $zdroje_ukazka = $vypisZdrojov->vratZdrojPodlaZdrojId($id);
 
         $this->data['zdroje'] = $zdroje; //aby sa dalo v pohlade pracovat s premennou zdroje
         $this->data['zdroje_ukazka'] = $zdroje_ukazka; //aby sa dalo v pohlade pracovat s premennou zdroje
-        $this->data['pouzivatel']=$pouzivatel; //prenesie do pohladu zdroje pole pouzivatel ktore obsahuje vsetky udaje z tabulky pouzivatel
+        $this->data['pouzivatel'] = $pouzivatel; //prenesie do pohladu zdroje pole pouzivatel ktore obsahuje vsetky udaje z tabulky pouzivatel
         $this->pohlad = 'zdroje';
-        
     }
-    
-    
+
 }
